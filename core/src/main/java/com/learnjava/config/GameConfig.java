5dde85fac32890011931ffcdb9523bdbfc7ebfb8
nazarchykur <nazarchykur@gmail.com>
2019-12-09 20:31:26 +0200

    5.21. Using Properties add file game.properties in resources/config folder inside GameConfig.java add this file as resource through @PropertySource("classpath:config/game.properties") and through @Value("${game.maxNumber:20}") on the field add this props w/ default value :20

--- core/src/main/java/com/learnjava/config/GameConfig.java
+++ core/src/main/java/com/learnjava/config/GameConfig.java
@@ -2,16 +2,21 @@ package com.learnjava.config;
 
 import com.learnjava.GuessCount;
 import com.learnjava.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields =
-    private int maxNumber = 50;
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
 
-    public int guessCount = 8;
+    @Value("${game.guessCount:5}")
+    public int guessCount;
 
     // == bean methods ==
     @Bean
