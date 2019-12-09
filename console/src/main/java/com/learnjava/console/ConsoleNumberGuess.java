05ce99024397c936b830e4a80b66452fbabe6a0a
nazarchykur <nazarchykur@gmail.com>
2019-12-09 15:50:34 +0200

    5.18. Finish Game Logic

--- console/src/main/java/com/learnjava/console/ConsoleNumberGuess.java
+++ console/src/main/java/com/learnjava/console/ConsoleNumberGuess.java
@@ -1,11 +1,16 @@
 package com.learnjava.console;
 
+import com.learnjava.Game;
+import com.learnjava.MessageGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
 
 @Component
 public class ConsoleNumberGuess  {
@@ -13,23 +18,37 @@ public class ConsoleNumberGuess  {
     // == const ==
     public static final Logger LOGGER = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
+    // == fields ==
+    @Autowired
+    private Game game;
+    @Autowired
+    private MessageGenerator messageGenerator;
 
     @EventListener(ContextRefreshedEvent.class)
     public void start() {
         LOGGER.info(" start() --> Container ready for use.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println(messageGenerator.getMainMessage());
+            System.out.println(messageGenerator.getResultMessage());
+
+            int guess = scanner.nextInt();
+            scanner.nextLine();
+            game.setGuess(guess);
+            game.check();
+
+            if (game.isGameWon() || game.isGameLost()) {
+                System.out.println(messageGenerator.getResultMessage());
+                System.out.println("Play again y/n?");
+
+                String playAgainString = scanner.nextLine().trim();
+                if (!playAgainString.equalsIgnoreCase("y")) {
+                    break;
+                }
+                game.reset();
+            }
+        }
     }
 }
-//
-//@Component
-//public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent>{
-//
-//    // == const ==
-//    public static final Logger LOGGER = LoggerFactory.getLogger(ConsoleNumberGuess.class);
-//
-//
-//    @Override
-//    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-//        LOGGER.info("Container ready for use.");
-//
-//    }
-//}
