2742e4549acfcc64fe24926da8258a099740d4dd
nazarchykur <nazarchykur@gmail.com>
2019-12-17 09:41:27 +0200

    4. Setup Thymeleaf and Initial Template

---
+++ web/src/main/java/com/learnsboot/util/ViewNames.java
@@ -0,0 +1,11 @@
+package com.learnsboot.util;
+
+public final class ViewNames {
+
+    // == const ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames() {
+    }
+}
