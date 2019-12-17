6b86297723e7dfb8579200f5f5a9fa520ea5f955
nazarchykur <nazarchykur@gmail.com>
2019-12-17 12:43:13 +0200

    6. add Game Service

---
+++ web/src/main/java/com/learnsboot/service/GameService.java
@@ -0,0 +1,14 @@
+package com.learnsboot.service;
+
+public interface GameService {
+
+    boolean isGameOver();
+
+    String getMainMessage();
+
+    String getResultMessage();
+
+    void checkGuess(int guess);
+
+    void reset();
+}
